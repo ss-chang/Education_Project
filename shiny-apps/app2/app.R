da07843d23c51295f38b187a2711d3535028fdb5
Shannon Chang <changxshannon@gmail.com>
2016-12-05 19:36:15 -0800

    displayed a sample of schools to increase datapoint visibility

--- shiny-apps/app2/app.R
+++ shiny-apps/app2/app.R
@@ -27,20 +27,25 @@ library(ggthemr)
 # Load relevant data and set-up data for app
 # =====================================================================================
 # read in your file, call it "dat"
-dat     <- read.csv("../../data/complete-data.csv", row.names = 1)
+dat                   <- read.csv("../../data/complete-data.csv", row.names = 1)
+dat                   <- dat[-which(duplicated(dat$INSTNM)), ]
+rownames(dat)         <- dat$INSTNM
+random_sample_smaller <- sample(1:nrow(dat), 50, replace = FALSE)
+dat                   <- dat[random_sample_smaller,]
 
 # data must be numeric for eda
 dat_eda <- dat[,-106]
 dat_eda <- apply(dat_eda, 2, function(x) as.numeric(x))
 dat_eda <- data.frame(dat_eda)
+row.names(dat_eda) <- row.names(dat)
 
 # get data ready for pca
 non_zero_var <- as.vector(sapply(dat_eda, function(x) var(x) != 0))
 nonzero_columns <- names(dat_eda)[non_zero_var]
 cols_to_keep <- names(dat_eda)[names(dat_eda) %in% nonzero_columns]
-cols_pca <- cols_to_keep[sample(length(cols_to_keep), 50)]
+
 # perform pca
-pca <- prcomp(dat_eda[,cols_pca], scale = T)
+pca <- prcomp(dat_eda[,cols_to_keep], scale = T)
 
 load("../../data/top_ten.RData")
 
@@ -56,9 +61,9 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       selectInput(inputId = "variable", 
-                  label = "Select a top-10 variable you would like to investigate!", 
+                  label = "Select a top-10 variable that you would like to investigate!", 
                   top_ten),
-      h3("Variable Index"),
+      h3("Variable Glossary"),
       p(span("INC_PCT_H1", style = "color:blue"),
         "is the share of students with family incomes between $75,001-$110,000 in nominal dollars."),
       p(span("PCTPELL", style = "color:blue"),
@@ -96,8 +101,8 @@ server <- function(input, output) {
     autoplot(pca, data=dat_eda, colour = selectedColumn(), alpha=.6, size=3, pch=4) + 
       ggtitle(paste0("PCA of ", selectedColumn())) +
       theme_wsj() + 
-      scale_colour_gradient(limits=c(0, 1), low="goldenrod", high="navyblue", space="Lab") + 
-      geom_text(label = dat$INSTNM)
+      geom_text(label = row.names(dat_eda), size = 3) + 
+      scale_colour_gradient(limits=c(0, 1), low="goldenrod", high="navyblue", space="Lab")
   })
 }
 shinyApp(server = server, ui = ui) 
